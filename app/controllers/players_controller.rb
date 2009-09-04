bd7c530b1bc749aa0b36d1d72b95f21e492f9696
Eladio Ruiz <eladioruiz@gmail.com>
2009-09-04 20:17:16 +0200

    fixing players controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,12 +1,17 @@
-class PlayersController < ApplicationController
-  # GET /players
-  # GET /players.xml
-  def index
-    @players = Player.all
+# Author::        Eladio Ruiz  (mailto:eladioruiz@gmail.com)
+# License::       Distributes under the same terms as Ruby
+# Last revision:: 04/09/2009 by Eladio Ruiz
+# Status::        Pending refactoring
+# Comments::      Pending testing
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @players }
+class PlayersController < ApplicationController
+  
+  # GET /players
+  def index
+    if params[:match_id] then
+      @players = Player.find_all_by_match_id(params[:match_id])
+    else
+      @players = Player.find(:all, :include => :match, :order => 'LOWER(matches.date_hour_match) DESC')
     end
   end
 
