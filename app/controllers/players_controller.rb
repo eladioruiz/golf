a66a70d891c294e445ce18194eea1c2e58f32a8e
Eladio Ruiz <eladioruiz@gmail.com>
2009-09-05 17:47:41 +0200

    resolving merge in app/controllers/players_controller.rb

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -8,33 +8,16 @@ class PlayersController < ApplicationController
   
   # GET /players
   def index
-<<<<<<< HEAD:app/controllers/players_controller.rb
-    @players = Player.all
- 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml { render :xml => @players }
-=======
     if params[:match_id] then
       @players = Player.find_all_by_match_id(params[:match_id])
     else
       @players = Player.find(:all, :include => :match, :order => 'LOWER(matches.date_hour_match) DESC')
->>>>>>> working_copy:app/controllers/players_controller.rb
     end
   end
  
   # GET /players/1
-  # GET /players/1.xml
   def show
     @player = Player.find(params[:id])
-<<<<<<< HEAD:app/controllers/players_controller.rb
- 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml { render :xml => @player }
-    end
-=======
->>>>>>> working_copy:app/controllers/players_controller.rb
   end
  
   # GET /players/new
@@ -48,14 +31,6 @@ class PlayersController < ApplicationController
     
     @tees = Tee.all
     @tees.map!{|tee| [tee.barras, tee.id]}
-<<<<<<< HEAD:app/controllers/players_controller.rb
- 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml { render :xml => @player }
-    end
-=======
->>>>>>> working_copy:app/controllers/players_controller.rb
   end
  
   # GET /players/1/edit
@@ -66,50 +41,24 @@ class PlayersController < ApplicationController
   # POST /players
   def create
     @player = Player.new(params[:player])
-<<<<<<< HEAD:app/controllers/players_controller.rb
- 
-    respond_to do |format|
-      if @player.save
-        flash[:notice] = 'Player was successfully created.'
-        format.html { redirect_to(@player) }
-        format.xml { render :xml => @player, :status => :created, :location => @player }
-      else
-        format.html { render :action => "new" }
-        format.xml { render :xml => @player.errors, :status => :unprocessable_entity }
-      end
-=======
 
     if @player.save
       flash[:notice] = 'Player was successfully created.'
       redirect_to(@player)
     else
       render :action => "new"
->>>>>>> working_copy:app/controllers/players_controller.rb
     end
   end
  
   # PUT /players/1
   def update
     @player = Player.find(params[:id])
-<<<<<<< HEAD:app/controllers/players_controller.rb
- 
-    respond_to do |format|
-      if @player.update_attributes(params[:player])
-        flash[:notice] = 'Player was successfully updated.'
-        format.html { redirect_to(@player) }
-        format.xml { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml { render :xml => @player.errors, :status => :unprocessable_entity }
-      end
-=======
 
     if @player.update_attributes(params[:player])
       flash[:notice] = 'Player was successfully updated.'
       redirect_to(@player)
     else
       render :action => "edit"
->>>>>>> working_copy:app/controllers/players_controller.rb
     end
   end
  
@@ -117,15 +66,7 @@ class PlayersController < ApplicationController
   def destroy
     @player = Player.find(params[:id])
     @player.destroy
-<<<<<<< HEAD:app/controllers/players_controller.rb
- 
-    respond_to do |format|
-      format.html { redirect_to(players_url) }
-      format.xml { head :ok }
-    end
-=======
 
     redirect_to(players_url)
->>>>>>> working_copy:app/controllers/players_controller.rb
   end
 end
