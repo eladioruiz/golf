1536df0e2000f31103a97dfafce68c764f6c3559
Eladio Ruiz <info@mygolfcard.es>
2010-07-09 20:41:30 +0200

    new match data display layout - graphical way

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,11 +15,35 @@ module ApplicationHelper
     date.strftime('%d/%m/%Y - %H:%M')
   end
 
+  def format_date_day(date)
+    date.strftime('%d/%m/%Y')
+  end
+
+  def format_date_hour(date)
+    date.strftime('%H:%M')
+  end
+
   def format_nil(value,default)
     if value.nil? then
       default
     else
       value
+    end
+  end
+
+  def course_image_default(course)
+    if course.images.empty? then
+      '/images/golf_03.gif'
+    else
+      course.images.first.fullName
+    end
+  end
+
+  def format_handicap(handicap)
+    if handicap.nil? then
+      ' - '
+    else
+      handicap.to_s()
     end
   end
 end
