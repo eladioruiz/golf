660c882b97e0c4a42d90a0c1e13b5d224b082801
Eladio Ruiz <info@mygolfcard.es>
2010-08-09 20:46:44 +0200

    mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,6 +14,17 @@ config.action_view.debug_rjs                         = true
 config.action_controller.perform_caching             = false
 
 # Don't care if the mailer can't send
-config.action_mailer.raise_delivery_errors = false
+config.action_mailer.raise_delivery_errors = true
 
 ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+ActionMailer::Base.smtp_settings = {
+    :tls => true,
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => "587",
+    :domain => "mygolfcard.es",
+    :authentication => :plain,
+    :user_name => "admin@mygolfcard.es",
+    :password => "Oidale27"
+  }
