065d733e2823db7e42ce93a4ee999928d94c34b1
Eladio Ruiz <info@mygolfcard.es>
2010-09-09 12:14:21 +0200

    getting course info

--- config/routes.rb
+++ config/routes.rb
@@ -46,7 +46,7 @@ ActionController::Routing::Routes.draw do |map|
 
   map.connect '/api/authentication', :controller => 'api', :action => 'authentication', :conditions => {:method => :post}
   map.connect '/api/getcourses', :controller => 'api', :action => 'getcourses'
-  map.connect '/api/infocourse', :controller => 'api', :action => 'infocourse', :conditions => {:method => :post}
+  map.connect '/api/getcourse', :controller => 'api', :action => 'infocourse', :conditions => {:method => :post}
   map.connect '/api/getmatches', :controller => 'api', :action => 'getmatches'
 
   map.resources :users
